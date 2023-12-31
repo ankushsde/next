6157a46e078da2fb43944939bcb484a33878f661
singhanku815@gmail.com <Ankush>
2024-01-01 02:46:10 +0530

    portfolio update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,16 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-export default function Home() {
+export default function Portfolio() {
+  const icons8ApiKey = 'YOUR_ICONS8_API_KEY';
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Header Section */}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started with Ankush Singh&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        {/* ... (existing code for header) */}
       </div>
 
+      {/* Hero Section */}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -39,75 +22,109 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* About Me Section */}
+      <section className="mb-8 text-center lg:max-w-5xl lg:w-full lg:mb-0">
+        <h1 className="text-4xl font-bold mb-4">Hello, I'm Ankush Singh!</h1>
+        <p className="text-lg opacity-70">
+          I'm a passionate software developer with expertise in building web applications using technologies like Next.js.
+        </p>
+      </section>
+
+      {/* Skills Section */}
+      <section className="mb-8 text-center lg:max-w-5xl lg:w-full lg:mb-0">
+        <h2 className="text-2xl font-semibold mb-4">Skills</h2>
+        <div className="flex justify-center space-x-4">
+          {/* Use Icons8 API for logos */}
+          
+          <Image
+            src={`https://img.icons8.com/color/48/000000/html-5--v1.png`}
+            alt="HTML"
+            width={40}
+            height={40}
+          />
+          <Image
+            src={`https://img.icons8.com/color/48/000000/css3.png`}
+            alt="CSS"
+            width={40}
+            height={40}
+          />
+          <Image
+            src={`https://img.icons8.com/color/48/000000/typescript.png`}
+            alt="TypeScript"
+            width={40}
+            height={40}
+          />
+          <Image
+            src={`https://img.icons8.com/color/48/000000/react-native.png`}
+            alt="React"
+            width={40}
+            height={40}
+          />
+          <Image
+            src={`https://img.icons8.com/plasticine/100/000000/react.png`}
+            alt="React Native"
+            width={40}
+            height={40}
+          />
+        </div>
+      </section>
+
+      {/* Social Profiles Section */}
+      <section className="mb-8 flex justify-center space-x-4">
+        {/* GitHub */}
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://github.com/ankushsde"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          <Image
+            src={`https://img.icons8.com/color/48/000000/github--v1.png`}
+            alt="GitHub"
+            width={40}
+            height={40}
+          />
+        </a>
+ 
+        
+
+        {/* CodeChef */}
+        <a
+          href="https://www.codechef.com/users/ankushs5252"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src={`https://img.icons8.com/color/48/000000/codechef.png`}
+            alt="CodeChef"
+            width={40}
+            height={40}
+          />
         </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        {/* AlgoDaily */}
+         
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      {/* Projects Section */}
+      <section className="mb-8 grid grid-cols-1 gap-8 lg:max-w-5xl lg:w-full lg:grid-cols-2">
+        {/* Add your project cards here with links to the projects */}
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Contact Section */}
+      <section className="text-center lg:max-w-5xl lg:w-full">
+        <h2 className="text-2xl font-semibold mb-4">Let's Connect!</h2>
+        <p className="opacity-70 mb-6">
+          If you're interested in collaborating or have any questions, feel free to reach out to me.
+        </p>
+        {/* Add your contact information or social media links */}
+      </section>
+
+      
+
+      {/* Footer Section */}
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        {/* ... (existing code for footer) */}
       </div>
     </main>
-  )
+  );
 }
